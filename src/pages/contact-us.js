2ce528b717a4c975b106d62fc7b2fe3d18c3c4e2
hyuna <kiipo0623@gmail.com>
2021-01-17 16:36:34 +0000

    card view

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import Card from './Card'
+import {Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
 
-const Contactus = () => {
+const useStyles = makeStyles({
+    gridContainer: {
+        paddingLeft: '20px',
+        paddingRight: '20px'
+    }
+  });
+
+function Contactus() {
+    const classes = useStyles();
     return (
-        <div style={{
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center', 
-            height: '90vh'}}>
-            <h1>Contact-us</h1>
-        </div>
+        //grid container를 사용하면 전체를 차지하지 않게 된다
+        //gutter 만들때 사용
+        <Grid container="container" className={classes.gridContainer} spacing={4}>
+            <Grid item="item" xs={12} sm={6} md={4}>
+                <Card/>
+            </Grid>
+            <Grid item="item" xs={2} sm={6} md={4}>
+                <Card/>
+            </Grid>
+            <Grid item="item" xs={2} sm={6} md={4}>
+                <Card/>
+            </Grid>
+            <Grid item="item" xs={2} sm={6} md={4}>
+                <Card/>
+            </Grid>
+        </Grid>
     )
 }
 
--- src/pages/service.js
+++ src/pages/service.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import StackGrid, {transitions} from 'react-stack-grid'
+import StackGrid, {transitions, easings} from 'react-stack-grid'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import styles from '../App.css'
